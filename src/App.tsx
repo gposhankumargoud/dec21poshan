b7358b54b63a2e0a0a1a5bd5e5332cd930d38c30
gposhankumargoud <gposhankumargoud@gmail.com>
2024-12-21 12:45:43 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import Home from './components/Home'
+import Movies from './components/Movie'
+export default function App() {
+  return (
+    <div><Home/><Movies/></div>
+  )
+}
